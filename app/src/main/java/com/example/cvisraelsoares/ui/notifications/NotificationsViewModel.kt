d1f16ff0ab748852d3d376fca53732ed2bd56d80
Israel Soares <raelvr@gmail.com>
2022-11-01 13:16:19 -0300

    First Commit

---
+++ app/src/main/java/com/example/cvisraelsoares/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cvisraelsoares.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
